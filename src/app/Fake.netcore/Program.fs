350c43e223e1033c8ab700cc840cbe937a9268cb
Matthias Dittrich <matthi.d@gmail.com>
2016-07-02 13:10:46 +0200

    use Argu for command line parsing && update to preview 2 tooling.

--- src/app/Fake.netcore/Program.fs
+++ src/app/Fake.netcore/Program.fs
@@ -5,7 +5,7 @@ open Fake.Trace
 open Fake.BuildServer
 open Fake.Fsi
 open System.IO
-open CommandLine
+open Argu
 
 let printVersion() =
     traceFAKE "FakePath: %s" fakePath
@@ -59,122 +59,124 @@ let splitCommandLine s =
   |> Seq.map (trim >> trimMatchingQuotes '\"')
   |> Seq.filter (System.String.IsNullOrEmpty >> not)
 
+
+let handleCli (results:ParseResult<Cli.FakeArgs>) =
+
+  let mutable exitCode = 0
+  let printDetails = results.Contains <@ Cli.FakeArgs.Verbose @>
+  if results.Contains <@ Cli.FakeArgs.Version @> then
+    printVersion()
+  results.IterResult (<@ Cli.FakeArgs.Run @>, fun runArgs ->
+    if runArgs.Contains <@ Cli.RunArgs.Debug @> then
+      Diagnostics.Debugger.Launch() |> ignore
+      Diagnostics.Debugger.Break() |> ignore
+
+    try
+      try
+        //AutoCloseXmlWriter <- true
+        //let cmdArgs = System.Environment.GetCommandLineArgs()
+        //match isBoot with
+        ////Boot.
+        //| true ->
+        //    let handler = Boot.HandlerForArgs bootArgs//Could be List.empty, but let Boot handle this.
+        //    handler.Interact()
+        //
+        ////Try and run a build script! 
+        //| false ->
+
+        traceStartBuild()
+        if printDetails then printVersion()
+
+        //Maybe log.
+        //match fakeArgs.TryGetResult <@ Cli.LogFile @> with
+        //| Some(path) -> addXmlListener path
+        //| None -> ()
+
+        //Combine the key value pair vars and the flag vars.
+        let envVars =
+            seq {
+              yield! 
+                runArgs.GetResults(<@ Cli.RunArgs.EnvironmentVariable @>)
+                //|> Seq.map (fun s -> let split = s.Split(':') in split.[0], split.[1])
+              if runArgs.Contains <@ Cli.RunArgs.SingleTarget @> then yield "single-target", "true"
+              if runArgs.Contains <@ Cli.RunArgs.Target @> then yield "target", runArgs.GetResult <@ Cli.RunArgs.Target @>
+            }
+            //seq { yield! fakeArgs.GetResults <@ Cli.EnvFlag @> |> Seq.map (fun name -> name, "true")
+            //      yield! fakeArgs.GetResults <@ Cli.EnvVar @>
+            //      if fakeArgs.Contains <@ Cli.Single_Target @> then yield "single-target", "true"
+            //      if args.Target.IsSome then yield "target", args.Target.Value }
+
+        //Get our fsiargs from somewhere!
+        let fsiArgLine = if runArgs.Contains <@ Cli.RunArgs.FsiArgs @> then runArgs.GetResult <@ Cli.RunArgs.FsiArgs @> else ""
+        let s = if runArgs.Contains <@ Cli.RunArgs.Script @> then Some (runArgs.GetResult <@ Cli.RunArgs.Script @>)  else None
+        let fsiArgs = 
+            match
+                splitCommandLine fsiArgLine |> Seq.toList,
+                s,
+                List.isEmpty buildScripts with
+
+            //TODO check for presence of --fsiargs with no args?  Make attribute for UAP?
+
+            //Use --fsiargs approach.
+            | x::xs, _, _ ->
+                match FsiArgs.parse (x::xs |> Array.ofList)  with
+                | Choice1Of2(fsiArgs) -> fsiArgs
+                | Choice2Of2(msg) -> failwith (sprintf "Unable to parse --fsiargs.  %s." msg)
+
+            //Script path is specified.
+            | [], Some(script), _ -> FsiArgs([], script, [])
+
+            //No explicit script, but have in working directory.
+            | [], None, false -> FsiArgs([], List.head buildScripts, [])
+
+            //Noooo script anywhere!
+            | [], None, true -> failwith "Build script not specified on command line, in fsi args or found in working directory."
+                  
+        //TODO if printDetails then printEnvironment cmdArgs args
+        let useCache = not (runArgs.Contains <@ Cli.RunArgs.NoCache @>)
+        if not (runBuildScriptWithFsiArgsAt printDetails fsiArgs envVars useCache) then exitCode <- 1
+        else if printDetails then log "Ready."
+      with
+      | exn ->
+          if printDetails then
+              sprintf "Build failed.\nError:\n%O" exn
+              |> traceError
+          else
+              if exn.InnerException <> null then
+                  sprintf "Build failed.\nError:\n%s\nInnerException:\n%s" exn.Message exn.InnerException.Message
+                  |> traceError
+                  //printUsage()
+              else
+                  sprintf "Build failed.\nError:\n%s" exn.Message
+                  |> traceError
+                  //printUsage()
+
+          //let isKnownException = exn :? FAKEException
+          //if not isKnownException then
+          //    sendTeamCityError exn.Message
+
+          exitCode <- 1
+
+      //killAllCreatedProcesses()
+
+    finally
+      traceEndBuild()
+  )
+  exitCode
+
 [<EntryPoint>]
 let main (args:string[]) =
   let mutable exitCode = 0
-  let mutable printDetails = false
+  let parser = ArgumentParser.Create<Cli.FakeArgs>("fake")
   try
-    try
-      //AutoCloseXmlWriter <- true
-      //let cmdArgs = System.Environment.GetCommandLineArgs()
-      let result = Parser.Default.ParseArguments<Cli.RunOptions, Cli.VersionOptions> args
-      match result with
-      | :? Parsed<obj> as command ->
-        match command.Value with
-        | :? Cli.VersionOptions as opts ->
-          //let isVersion = false // version switch is no a different 'verb'
-          printVersion()
-        | :? Cli.RunOptions as opts ->
-          //Break to allow a debugger to be attached here
-          if opts.debugScript then
-              Diagnostics.Debugger.Launch() |> ignore
-              Diagnostics.Debugger.Break() |> ignore
-
-          //Boot and version force us to ignore other args, so check for them and handle.
-          //let isBoot, bootArgs = false, [] // no boot support (whatever that is)
-          printDetails <- opts.verbose
-
-          //match isBoot with
-          ////Boot.
-          //| true ->
-          //    let handler = Boot.HandlerForArgs bootArgs//Could be List.empty, but let Boot handle this.
-          //    handler.Interact()
-          //
-          ////Try and run a build script! 
-          //| false ->
-
-          traceStartBuild()
-          if printDetails then printVersion()
-
-          //Maybe log.
-          //match fakeArgs.TryGetResult <@ Cli.LogFile @> with
-          //| Some(path) -> addXmlListener path
-          //| None -> ()
-
-          //Combine the key value pair vars and the flag vars.
-          let envVars =
-              seq {
-                yield! 
-                  opts.environmentVariables
-                  |> Seq.map (fun s -> let split = s.Split(':') in split.[0], split.[1])
-                if opts.singleTarget then yield "single-target", "true"
-                if opts.target.IsSome then yield "target", opts.target.Value
-              }
-              //seq { yield! fakeArgs.GetResults <@ Cli.EnvFlag @> |> Seq.map (fun name -> name, "true")
-              //      yield! fakeArgs.GetResults <@ Cli.EnvVar @>
-              //      if fakeArgs.Contains <@ Cli.Single_Target @> then yield "single-target", "true"
-              //      if args.Target.IsSome then yield "target", args.Target.Value }
-
-          //Get our fsiargs from somewhere!
-          let fsiArgs = 
-              match
-                  splitCommandLine opts.fsiArgs |> Seq.toList,
-                  opts.script,
-                  List.isEmpty buildScripts with
-
-              //TODO check for presence of --fsiargs with no args?  Make attribute for UAP?
-
-              //Use --fsiargs approach.
-              | x::xs, _, _ ->
-                  match FsiArgs.parse (x::xs |> Array.ofList)  with
-                  | Choice1Of2(fsiArgs) -> fsiArgs
-                  | Choice2Of2(msg) -> failwith (sprintf "Unable to parse --fsiargs.  %s." msg)
-
-              //Script path is specified.
-              | [], Some(script), _ -> FsiArgs([], script, [])
-
-              //No explicit script, but have in working directory.
-              | [], None, false -> FsiArgs([], List.head buildScripts, [])
-
-              //Noooo script anywhere!
-              | [], None, true -> failwith "Build script not specified on command line, in fsi args or found in working directory."
-                    
-          //TODO if printDetails then printEnvironment cmdArgs args
-
-          let useCache = not opts.noCache
-          if not (runBuildScriptWithFsiArgsAt printDetails fsiArgs envVars useCache) then exitCode <- 1
-          else if printDetails then log "Ready."
-
-      | :? NotParsed<obj> ->
-        exitCode <- 1
-
-    with
-    | exn ->
-        if printDetails then
-            sprintf "Build failed.\nError:\n%O" exn
-            |> traceError
-        else
-            if exn.InnerException <> null then
-                sprintf "Build failed.\nError:\n%s\nInnerException:\n%s" exn.Message exn.InnerException.Message
-                |> traceError
-                //printUsage()
-            else
-                sprintf "Build failed.\nError:\n%s" exn.Message
-                |> traceError
-                //printUsage()
-
-        //let isKnownException = exn :? FAKEException
-        //if not isKnownException then
-        //    sendTeamCityError exn.Message
-
-        exitCode <- 1
-
-    //killAllCreatedProcesses()
-
-  finally
-    traceEndBuild()
-    //if !TargetHelper.ExitCode.exitCode <> 0 then exit !TargetHelper.ExitCode.exitCode
+    let results = parser.Parse(args)
+    exitCode <- handleCli results
+  with
+  | :? ArguParseException as e ->
+    printfn "%s" e.Message
+    exitCode <- 1
 #if !NETSTANDARD1_5
-    if Environment.ExitCode <> 0 then exitCode <- Environment.ExitCode
+  //if !TargetHelper.ExitCode.exitCode <> 0 then exit !TargetHelper.ExitCode.exitCode
+  if Environment.ExitCode <> 0 then exitCode <- Environment.ExitCode
 #endif
   exitCode
