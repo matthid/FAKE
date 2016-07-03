0db98e0f03450953f9d59dc0b045c9d93c4cd873
Matthias Dittrich <matthi.d@gmail.com>
2016-07-04 00:45:20 +0200

    update to fixed paket.

--- src/app/Fake.netcore/unusedDependencies.fsx
+++ src/app/Fake.netcore/unusedDependencies.fsx
@@ -3,6 +3,7 @@ source https://nuget.org/api/v2
 
 nuget System.Xml.XmlDocument ~> 4.0
 nuget System.Security.Cryptography.Algorithms ~> 4.0
+nuget Microsoft.FSharp.Core.netcore
 -- Fake Dependencies -- *)
 #load "./.fake/unusedDependencies.fsx/loadDependencies.fsx"
 
