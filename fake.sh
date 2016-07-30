b38c6966007a78667b46092d519ba20a24e6cacf
Matthias Dittrich <matthi.d@gmail.com>
2016-07-30 22:08:44 +0200

    use obtain_fake everywhere.

--- fake.sh
+++ fake.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-p=".fake";f="$p/fake_obtain.sh";if [ ! -f "$f" ]; then mkdir $p; curl --fail -L -s -o $f https://raw.githubusercontent.com/matthid/FAKE/coreclr/script/obtain_fake.sh; fi; . $f
+p=".fake";f="$p/obtain_fake.sh";if [ ! -f "$f" ]; then mkdir $p; curl --fail -L -s -o $f https://raw.githubusercontent.com/matthid/FAKE/coreclr/script/obtain_fake.sh; fi; . $f
 
 exec_fake $*
