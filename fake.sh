f38d252e081d60997d8c7987a53c460d54a0d1d6
Matthias Dittrich <matthi.d@gmail.com>
2016-07-31 10:07:24 +0200

    be more verbose

--- fake.sh
+++ fake.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # p=".fake";f="$p/obtain_fake.sh";if [ ! -f "$f" ]; then mkdir -p $p; curl --fail -L -s -o $f https://raw.githubusercontent.com/matthid/FAKE/coreclr/script/obtain_fake.sh; fi; . $f
+VERBOSE=true
+
 . script/obtain_fake.sh
 
 install_fake_packages
