bfb4527d3956caaa6dc50f757f3cbf521848b06a
Matthias Dittrich <matthi.d@gmail.com>
2016-07-31 12:36:55 +0200

    print all vars

--- fake.sh
+++ fake.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # p=".fake";f="$p/obtain_fake.sh";if [ ! -f "$f" ]; then mkdir -p $p; curl --fail -L -s -o $f https://raw.githubusercontent.com/matthid/FAKE/coreclr/script/obtain_fake.sh; fi; . $f
+
+#print environment info
+env
+
 VERBOSE=true
 FAKE_VERSION="core-v1.0-alpha-06"
 
